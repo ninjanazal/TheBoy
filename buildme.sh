9ad404b2553fab52f4c751301e9e4ff389d7c112
Eurico Martins <euricomartinss@gmail.com>
2022-04-21 22:31:47 +0100

    Included submodules, updated build script

--- buildme.sh
+++ buildme.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 echo -e "[SCRIPT] Regenerating the project"
 
-
 if [ ! -d "Build" ]; then
-  echo -e "[SCRIPT] Build folder not found"
-  mkdir Build
+	echo -e "[SCRIPT] Build folder not found"
+	mkdir Build
 fi
 
+echo -e "[SCRIPT] Building SFML Module!"
+if [ ! -d "Include" ]; then
+	cd SFML
+	mkdir Build && cd Build
+	echo -e "[SCRIPT] Generating SFML Compiling properties!"
+	cmake ..	
+	cmake -DBUILD_SHARED_LIBS=FALSE ..
+	cmake --build .
+	cd ../..
+
 cd Build
-echo -e "[SCRIPT] Regenerating Project"
+echo -e "[SCRIPT] Generating compilation Project"
 cmake ..
 
 echo -e "[SCRIPT] Building Project"
